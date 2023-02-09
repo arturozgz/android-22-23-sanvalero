71b48692e14690dcbe29bc791ec2726bd9b0ab38
arturozgz <123074970+arturozgz@users.noreply.github.com>
2023-02-09 09:43:27 +0100

    Delete Skills5/Napoles_Avanzado_Retrofit directory

--- Skills5/Napoles_Avanzado_Retrofit/app/src/main/java/com/example/napoles_avanzado_retrofit/model/API_KEY.java
+++
@@ -1,4 +0,0 @@
-package com.example.napoles_avanzado_retrofit.model;
-
-public class API_KEY {
-}
--- Skills5/Napoles_Avanzado_Retrofit/app/src/main/java/com/example/napoles_avanzado_retrofit/model/ApiInterface.java
+++
@@ -1,4 +0,0 @@
-package com.example.napoles_avanzado_retrofit.model;
-
-public class ApiInterface {
-}
